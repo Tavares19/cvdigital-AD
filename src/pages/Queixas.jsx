1a11200408bfc09eb4136d336321c33eadd48476
qsunicv <66311000+qsunicv@users.noreply.github.com>
2022-02-04 13:45:15 -0100

    update

--- src/pages/Queixas.jsx
+++ src/pages/Queixas.jsx
@@ -8,51 +8,7 @@ const Queixas = () => {
         <div>
 
         <h1>Declarar uma Queixas</h1>
-        <fieldset class="grupo">
-                      <div class="campo">
-                          <label for="nome">Nome</label>
-                          <input type="text" id="nome" name="nome" />
-                      </div>
-                      <div class="campo">
-                          <label for="snome">Sobrenome</label>
-                          <input type="text" id="nome" name="nome" />
-                      </div>
-                  </fieldset>
-                  <div class="campo">
-                      <label>Sexo</label>
-                      <label>
-                          <input type="radio" name="sexo" value="masculino"/> Masculino
-                      </label>
-                      <label>
-                          <input type="radio" name="sexo" value="feminino"/> Feminino
-                      </label>
-                  </div>
-                  <div class="campo">
-                      <label for="email">E-mail</label>
-                      <input type="text" id="email" name="email"  value=""/>
-                  </div>
-                  <div class="campo">
-                      <label for="telefone">Nº de associados</label>
-                      <input type="text" id="telefone" name="telefone"  value=""/>
-                  </div>
-            
-                  <fieldset class="grupo">
-                      <div class="campo">
-                          <label for="cidade">Tempo na empresa</label>
-                          <input type="text" id="cidade" name="cidade"  value=""/>
-                      </div>
-                      <div class="campo">
-                          <label for="estado">Categoria</label>
-                          <select name="estado" id="estado">
-                              <option value="">-------------------</option>
-                              <option value="PR">socio efetivo</option>
-                              <option value="PR">socio não efetivo</option>
-                              <option value="PR">socio colaborador</option>
-                              <option value="PR">socio benemérito</option>
-                              <option value="PR">socio honorário</option>
-                          </select>
-                      </div>
-                  </fieldset>
+
             
                   <div class="campo resumo">
                       <label for="mensagem">Motivos Da Queixa</label>
